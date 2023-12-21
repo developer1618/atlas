51da77c9643a39d098847909921d8613839f15b0
1skoa <aminoviskandar0@gmail.com>
2023-12-21 16:47:10 +0500

    test

--- platform/plugins/hotel/src/Http/Controllers/PublicController.php
+++ platform/plugins/hotel/src/Http/Controllers/PublicController.php
@@ -449,13 +449,12 @@ class PublicController extends Controller
             $user = Auth::guard('customer')->user();
             $selectedServices = Service::whereIn('id', $serviceIds)->get();
             $currentBonuses = $user->bonuses;
-            
+
             // Добавление бонусов за комнату к общему балансу бонусов пользователя
             $user->update([
                 'bonuses' => $user->bonuses + $roomBonus,
             ]);
             $isBonusUsed = $request->input('bonus', 0);
-            // Проверяем, достаточно ли у пользователя бонусов для оплаты услуг
             if ($isBonusUsed == 1) {
                 // Проверяем, достаточно ли у пользователя бонусов для оплаты услуг
                 if (!$selectedServices->isEmpty()) {
